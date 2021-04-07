74ede75f5d6c996aff7e28eae1c5b9f3434f50a6
Jesse White <jesse.white6@gmail.com>
2021-04-07 16:27:58 -0700

    add business logic and will make the game work without user interface.
    Co-authored-by: Thomas Russell <t.p.russell19@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,79 +1,45 @@
-function PlayerObject(playerName, totalScore, tempScore) {
+function PlayerObject(playerName) {
     this.playerName = playerName;
-    this.totalScore = totalScore;
-    this.tempScore = tempScore;
-  }
-  
-  function switchPlayer() {
+    this.totalScore = 0;
+    this.tempScore = 0;
+}
+
+let playerOne = new PlayerObject("player1");
+let playerTwo = new PlayerObject("player2");
+
+let currentPlayer = playerOne;
+
+PlayerObject.prototype.switchPlayer = function () {
     if (currentPlayer === playerOne) {
-      currentPlayer = playerTwo;
+        currentPlayer = playerTwo;
     } else {
-      currentPlayer = playerOne;
+        currentPlayer = playerOne;
     }
     currentPlayer.tempScore = 0;
-  }
-  
-  
-  PlayerObject.prototype.rollDice = function() {
-    let currentRoll = Math.floor(Math.random() * 6) + 1 //random number
+}
+
+PlayerObject.prototype.rollDice = function () {
+    let currentRoll = Math.floor(Math.random() * 6) + 1;
+    console.log("Player: " + currentPlayer.playerName, "Current Roll: " + currentRoll, "Temp Score: " + currentPlayer.tempScore, "Total Score: " + currentPlayer.totalScore); //random number
     if (currentRoll === 1) { //return if 1
-      return totalScore;
+        currentPlayer.switchPlayer();
     } else {
-      this.tempScore += currentRoll; //add if !one
-      let myChoice = prompt("Would you like to roll again?")
-      if (myChoice) {
-        rolldice
-      } else {
-        totaSCore += tempScore;
-        playerSwitch;
-      }
-  
-  
-      /*     } else */
-      /* Promt user to hold or roll again? 
-      if hold is called then we need to add tempScore to Total score.
-      When do we swithch players 
-      */
-  
-  
-  
-  
-    }
-  }
-  
-  
-  
-  
-  
-  
-  //roll random number between 1-6 random
-  //branch based on results of 1 || any other
-  //break out on 1
-  //add to score on any other
-  //option to end turn on hold
-  
-  
-  //two objects per player
-  // object will trask total score and score to add as well as score to remove
-  //player 1 and player name input
-  /* Roll button adds points to temp var until 1 is tolled or hold button is pushed */
-  //let tempScore = inboundPlayerScore
-  //bool firstTime = true;
-  /* 
-  playerObject.rollDice = function() {
-    
-    if(firstTIme === true) {
-      tempScore = score;
-      firstTime = false;
-    }
-    
-    let randomNum = (math.random(floor-6))
-    if(randomNum === 1) {
-      return score;
-    } else {
-      tempScore += randomNum;
-      
-    }
-    
-    
-  } */
+        currentPlayer.tempScore += currentRoll;
+    } return currentPlayer;
+}
+
+$(document).ready(function () {
+    $("#roll-dice").click(function (event) {
+        event.preventDefault();
+        currentPlayer.rollDice();
+    })
+    $("#hold").click(function (event) {
+        event.preventDefault();
+        currentPlayer.totalScore += currentPlayer.tempScore;
+        currentPlayer.switchPlayer();
+    })
+    const playerOneScore = playerOne;
+    const playerTwoScore = playerTwo;
+    $("#player-one").text(`Player One Info: ${playerOneScore}`);
+    $("#player-two").text(`Player Two Info: ${playerTwoScore}`);
+})
