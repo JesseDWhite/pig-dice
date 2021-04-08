6160658b2c1a268fca18a12fbea3419978eaa387
Jesse White <jesse.white6@gmail.com>
2021-04-08 16:41:57 -0700

    add dice animation and animation for 1 being rolled.
    Co-authored-by: Thomas Russell <t.p.russell19@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+function allPlayerCallOuts() {
+    playerOneHTML();
+    playerTwoHTML();
+    currentPlayerHTML();
+    diceImage();
+}
+
 function PlayerObject(playerName) {
     this.playerName = playerName;
     this.totalScore = 0;
@@ -6,6 +13,7 @@ function PlayerObject(playerName) {
 
 let playerOne = new PlayerObject("player1");
 let playerTwo = new PlayerObject("player2");
+let computer = new PlayerObject("computer")
 
 let currentPlayer = playerOne;
 let currentRoll = 0;
@@ -20,8 +28,7 @@ PlayerObject.prototype.switchPlayer = function () {
 }
 
 PlayerObject.prototype.rollDice = function () {
-    currentRoll = Math.floor(Math.random() * 6) + 1;
-    console.log("Player: " + currentPlayer.playerName, "Current Roll: " + currentRoll, "Temp Score: " + currentPlayer.tempScore, "Total Score: " + currentPlayer.totalScore); //random number
+    currentRoll = Math.floor(Math.random() * 6) + 1; //random number
     if (currentRoll === 1) { //return if 1
         currentPlayer.switchPlayer();
     } else {
@@ -29,20 +36,30 @@ PlayerObject.prototype.rollDice = function () {
     } return currentPlayer;
 }
 
+function computerHold() {
+    currentPlayer.totalScore += currentPlayer.tempScore;
+    currentPlayer.switchPlayer();
+}
+
+PlayerObject.prototype.computerRollDice = function () {
+    let computerArray = [computerRollDice(), computerHold()]
+    currentRoll = Math.floor(Math.random() * 6) + 1; //random number
+    if (currentRoll === 1) { //return if 1
+        currentPlayer.switchPlayer();
+    } else {
+        currentPlayer.tempScore += currentRoll;
+        const random = Math.floor(Math.random() * computerArray.length - 1)
+        computerArray[random];
+        console.log(random);
+    } return currentPlayer;
+}
+
 function playerOneHTML() {
-    $("#player-one").html(`Player One Info:<br>Total score: ${playerOne.totalScore}<br>Current turn: ${playerOne.tempScore}`)
+    $("#player-one").html(`Total score: ${playerOne.totalScore}<br>Current turn: ${playerOne.tempScore}`)
 };
 
 function playerTwoHTML() {
-    $("#player-two").html(`Player Two Info:<br>Total score: ${playerTwo.totalScore}<br>Current turn: ${playerTwo.tempScore}`)
-};
-
-function currentRollHTML() {
-    if (currentRoll === 1) {
-        $("#current-roll").html("BUST!!!");
-    } else {
-        $("#current-roll").html(`Current roll: ${currentRoll}`);
-    };
+    $("#player-two").html(`Total score: ${playerTwo.totalScore}<br>Current turn: ${playerTwo.tempScore}`)
 };
 
 function currentPlayerHTML() {
@@ -55,36 +72,56 @@ function currentPlayerHTML() {
     };
 };
 
-$(document).ready(function () {
-    playerOneHTML();
-    playerTwoHTML();
-    currentRollHTML();
-    currentPlayerHTML();
+function diceImage() {
+    if (currentRoll === 1) {
+        $("img").attr("src", "/img/one.png")
+        $(".dice-glow").attr("id", "dice-glow");
+    } else if (currentRoll === 2) {
+        $("img").attr("src", "/img/two.png")
+        $("#dice-glow").removeAttr("id");
+    } else if (currentRoll === 3) {
+        $("img").attr("src", "/img/three.png")
+        $("#dice-glow").removeAttr("id");
+    } else if (currentRoll === 4) {
+        $("img").attr("src", "/img/four.png")
+        $("#dice-glow").removeAttr("id");
+    } else if (currentRoll === 5) {
+        $("img").attr("src", "/img/five.png")
+        $("#dice-glow").removeAttr("id");
+    } else if (currentRoll === 6) {
+        $("img").attr("src", "/img/six.png")
+        $("#dice-glow").removeAttr("id");
+    } else {
+        $("#dice-glow").removeAttr("id");
+    }
+}
 
+$(document).ready(function () {
+    // $("#select-player").submit(function (event) {
+    //     event.preventDefault()
+
+    allPlayerCallOuts()
+    // console.log(parseInt($("#player-select").val()));
+    // if (parseInt($("#player-select").val() === 2)) {
     $("#roll-dice").click(function (event) {
         event.preventDefault();
         currentPlayer.rollDice();
-        playerOneHTML();
-        playerTwoHTML();
-        currentRollHTML();
-        currentPlayerHTML();
+        allPlayerCallOuts()
         if (currentPlayer.tempScore + currentPlayer.totalScore >= 100) {
             if (alert(`${currentPlayer.playerName}, you won!!! Hit OK to play again!`)) { }
             else {
                 window.location.reload();
             }
         };
-    });
+    })
     $("#hold").click(function (event) {
         event.preventDefault();
         currentPlayer.totalScore += currentPlayer.tempScore;
         currentPlayer.switchPlayer();
-        playerOneHTML();
-        playerTwoHTML();
-        currentRollHTML();
-        currentPlayerHTML();
-    });
+        allPlayerCallOuts()
+    })
 });
+
 
 
 
